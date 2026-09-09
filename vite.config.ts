aeb9d2ed0eab48c0dd49a6fb2f9bcfb43b3ef45b
iannarella1 <miguel02012008@gmail.com>
2026-09-09 02:30:51 -0300

    Configura GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
-  base: './',
+  base: '/Nexora/',
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
+
   server: {
     port: 5173,
   },
